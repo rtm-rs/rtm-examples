f624f0cdf7137e008cac63524600dff2ba634acd
Begley Brothers Inc <begleybrothers@gmail.com>
2022-05-18 13:55:35 +1000

    Evolve[DB]: Migrate to version 28

--- crates/cqrs03/entities/src/products.rs
+++ crates/cqrs03/entities/src/products.rs
@@ -15,8 +15,6 @@ impl EntityName for Entity {
 #[derive(Clone, Debug, PartialEq, DeriveModel, DeriveActiveModel, Serialize, Deserialize)]
 pub struct Model {
     pub name: Option<String>,
-    pub created_at: DateTimeUtc,
-    pub updated_at: DateTimeUtc,
     pub price: Option<Decimal>,
     pub id: Uuid,
     pub stock_level: Option<i32>,
@@ -26,8 +24,6 @@ pub struct Model {
 #[derive(Copy, Clone, Debug, EnumIter, DeriveColumn)]
 pub enum Column {
     Name,
-    CreatedAt,
-    UpdatedAt,
     Price,
     Id,
     StockLevel,
@@ -54,8 +50,6 @@ impl ColumnTrait for Column {
     fn def(&self) -> ColumnDef {
         match self {
             Self::Name => ColumnType::String(None).def().null(),
-            Self::CreatedAt => ColumnType::Timestamp.def(),
-            Self::UpdatedAt => ColumnType::Timestamp.def(),
             Self::Price => ColumnType::Decimal(Some((8u32, 2u32))).def().null(),
             Self::Id => ColumnType::Uuid.def(),
             Self::StockLevel => ColumnType::Integer.def().null(),
