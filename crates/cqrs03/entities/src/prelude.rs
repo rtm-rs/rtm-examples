62b8eb01bec15061602a8be8a4648eaaf8c8bc34
Begley Brothers Inc <begleybrothers@gmail.com>
2022-05-18 13:55:41 +1000

    Evolve[DB]: Migrate to version 30

--- crates/cqrs03/entities/src/prelude.rs
+++ crates/cqrs03/entities/src/prelude.rs
@@ -6,6 +6,7 @@ pub use super::event_store_events::Entity as EventStoreEvents;
 pub use super::event_store_events_in_streams::Entity as EventStoreEventsInStreams;
 pub use super::order_lines::Entity as OrderLines;
 pub use super::orders::Entity as Orders;
+pub use super::orders_products::Entity as OrdersProducts;
 pub use super::products::Entity as Products;
 pub use super::refinery_schema_history::Entity as RefinerySchemaHistory;
 pub use super::schema_migrations::Entity as SchemaMigrations;
